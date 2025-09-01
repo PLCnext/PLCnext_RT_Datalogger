5a6b6719d4911d0424927e72ada721d6174bcab5
Eduard Münz <32840765+emuenz@users.noreply.github.com>
2025-09-01 14:50:21 +0200

    Update CppDataLoggerComponent.hpp
    Added:
    using namespace Arp::System::Commons::Threading;

--- getting-started/Part-05/src/CppDataLoggerComponent.hpp
+++ getting-started/Part-05/src/CppDataLoggerComponent.hpp
@@ -29,6 +29,7 @@ using namespace Arp;
 using namespace Arp::System::Acf;
 using namespace Arp::Plc::Commons::Esm;
 using namespace Arp::Plc::Commons::Meta;
+using namespace Arp::System::Commons::Threading;
 
 using namespace Arp::System::Rsc;
 using namespace Arp::System::Rsc::Services;
@@ -147,3 +148,4 @@ inline IComponent::Ptr CppDataLoggerComponent::Create(Arp::System::Acf::IApplica
 }
 
 } // end of namespace CppDataLogger
+
